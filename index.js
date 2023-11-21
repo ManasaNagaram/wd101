350e4d0a1a4d8a9e9c75c02c4b7f07e9b8776340
Manasa <119737682+ManasaNagaram@users.noreply.github.com>
2023-11-21 23:49:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+
+let userForm = document.getElementById('userform');
+
+const retrieveEntries = () => {
+    let entries = localStorage.getItem("user-entries");
+    if (entries) {
+        entries = JSON.parse(entries);
+    } else {
+        entries = [];
+    }
+    return entries;
+}
+
+let userEntries = retrieveEntries();
+
+const displayEntries = () => {
+    const entries = retrieveEntries();
+
+    const tableEntries = entries.map((entry) => {
+        const nameCell = `<td>${entry.name}</td>`;
+        const emailCell = `<td>${entry.email}</td>`;
+        const passwordCell = `<td>${entry.password}</td>`;
+        const dobCell = `<td>${entry.dob}</td>`;
+        const acceptedTermsCell = `<td>${entry.acceptedTerms}</td>`;
+        return `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptedTermsCell}</tr>`;
+    }).join('\n');
+
+    const table = `<table><tr>
+        <th>Name </th>
+        <th>Email </th>
+        <th>Password  </th>
+        <th> Dob </th>
+        <th>Accepted Terms? </th>
+    </tr>${tableEntries}</table>`;
+
+    let details = document.getElementById("user-entries");
+    details.innerHTML = table;
+}
+
+const saveUserForm = (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const acceptedTerms = document.getElementById('acceptTerms').checked;
+
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTerms
+    };
+
+    userEntries.push(entry);
+    localStorage.setItem("user-entries", JSON.stringify(userEntries));
+    displayEntries();
+}
+
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
